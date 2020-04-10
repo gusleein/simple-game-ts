d0861aaed4aa572b19fa8ea12cd74abd25632a94
somna <free_apples@ymail.com>
2020-04-10 09:01:00 +0700

    code for the asterods game

--- src/components/AsteroidsGame/lib/Game.class.ts
+++ src/components/AsteroidsGame/lib/Game.class.ts
@@ -1,4 +1,5 @@
 export interface Game {}
+
 export class Game implements Game {
   ctx?: CanvasRenderingContext2D;
   lastRender: number = 0;
@@ -7,8 +8,15 @@ export class Game implements Game {
   height: number = 300;
 
   state: State = {
-    x: this.width / 2,
-    y: this.height / 2,
+    position: {
+      x: this.width / 2,
+      y: this.height / 2,
+    },
+    movement: {
+      x: 0,
+      y: 0
+    },
+    rotation: 0,
     pressedKeys: {
       'left': false,
       'right': false,
@@ -38,31 +46,92 @@ export class Game implements Game {
   }
 
   update(progress: number) {
-    if (this.state.pressedKeys.left)
-      this.state.x -= progress;
-    if (this.state.pressedKeys.right)
-      this.state.x += progress;
-    if (this.state.pressedKeys.up)
-      this.state.y -= progress;
-    if (this.state.pressedKeys.down)
-      this.state.y += progress;
+    const p = progress / 16
 
-    if (this.state.x > this.width) 
-      this.state.x -= this.width;
-    if (this.state.x < 0) 
-      this.state.x += this.width;
-    if (this.state.y > this.height) 
-      this.state.y -= this.height;
-    if (this.state.y < 0) 
-      this.state.y += this.height;
-    
+    this.updateRotation(p)
+    this.updateMovement(p)
+    this.updatePosition(p)
+  }
+
+  updateRotation(p: number) {
+    if (this.state.pressedKeys.left) { 
+      this.state.rotation -= p * 5
+    }
+    else if (this.state.pressedKeys.right) {
+      this.state.rotation += p * 5
+    }
+  }
+
+  updateMovement(p: number) {
+    const state = this.state
+    const accelerationVector = {
+      x: p * .2 * Math.cos((state.rotation - 90) * (Math.PI/180)),
+      y: p * .2 * Math.sin((state.rotation - 90) * (Math.PI/180))
+    }
+    const speedLimit = 5
+
+    if (state.pressedKeys.up) {
+      state.movement.x += accelerationVector.x;
+      state.movement.y += accelerationVector.y;
+    } else if (state.pressedKeys.down) {
+      state.movement.x -= accelerationVector.x;
+      state.movement.y -= accelerationVector.y;
+    }
+
+    // limit movement speed
+    if (state.movement.x > speedLimit) {
+      state.movement.x = speedLimit;
+    } else if (state.movement.x < -speedLimit) {
+      state.movement.x = -speedLimit
+    }
+
+    if (state.movement.y > speedLimit) {
+      state.movement.y = speedLimit
+    } else if (state.movement.y < -speedLimit) {
+      state.movement.y = -speedLimit
+    }
+  }
+
+  updatePosition(p: number) {
+    const state = this.state;
+
+    state.position.x += state.movement.x
+    state.position.y += state.movement.y
+
+    // detect boundaries
+    if (state.position.x > this.width) {
+      state.position.x -= this.width
+    } else if (state.position.x < 0) {
+      state.position.x += this.width
+    }
+
+    if (state.position.y > this.height) {
+      state.position.y -= this.height
+    } else if (state.position.y < 0) {
+      state.position.y += this.height
+    }
   }
 
   draw() {
     if (this.ctx) {
-      this.ctx.fillStyle = "red"
-      this.ctx.clearRect(0, 0, this.width, this.height);
-      this.ctx.fillRect(this.state.x - 5, this.state.y - 5, 10, 10)
+      const ctx = this.ctx
+      ctx.clearRect(0, 0, this.width, this.height)
+
+      ctx.save()
+      ctx.translate(this.state.position.x, this.state.position.y)
+      ctx.rotate((Math.PI/180) * this.state.rotation)
+
+      ctx.strokeStyle = 'white'
+      ctx.lineWidth = 2
+      ctx.beginPath()
+      ctx.moveTo(0, 0)
+      ctx.lineTo(5, 5)
+      ctx.lineTo(0, -10)
+      ctx.lineTo(-5, 5)
+      ctx.lineTo(0, 0)
+      ctx.closePath()
+      ctx.stroke()
+      ctx.restore()
     }
   }
 
@@ -87,8 +156,9 @@ export class Game implements Game {
   }
 }
 interface State {
-  x: number,
-  y:  number,
+  position: {x: number, y: number},
+  movement: {x: number, y: number},
+  rotation: number,
   pressedKeys: PressedKeys,
 }
 
