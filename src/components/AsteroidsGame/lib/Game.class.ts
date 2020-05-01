73b35edaeadfde02b7840b053ab4fcc544c6b83e
somna <free_apples@ymail.com>
2020-05-02 02:05:01 +0700

    add classes: CoreEngine, Game, GameScene, InputHandler, PlayerShip, RenderingEngine

---
+++ src/components/AsteroidsGame/lib/Game.class.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by @gusleein (Andrey Sanatullov)
+ * https://github.com/gusleein
+ *
+ * on 02/05/2020.
+ */
+
+export default class Game {
+  gameObjects = [];
+  gameIsStopped = true;
+  asteroidsCount = 0;
+  playersCount = 1;
+
+  constructor() {
+    this.init()
+  }
+
+  init() {
+
+  }
+
+  addGameObject() {
+
+  }
+}
---
+++ src/components/AsteroidsGame/lib/PlayerShip.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by @gusleein (Andrey Sanatullov)
+ * https://github.com/gusleein
+ *
+ * on 02/05/2020.
+ */
+
+export default class PlayerShip {
+
+}
